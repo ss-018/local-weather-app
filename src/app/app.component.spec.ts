d90e7590ef8f50678783146170e9cce8d8822026
Sarjodh Dhillon <sarjodhdhillon@Sarjodhs-MacBook-Pro.local>
2020-06-06 19:48:31 -0700

    Added configurations for styling

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,32 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { TestBed, async } from "@angular/core/testing";
 
-describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
+import { AppComponent } from "./app.component";
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+describe("AppComponent", () => {
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [AppComponent],
+		}).compileComponents();
+	}));
 
-  it(`should have as title 'local-weather-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('local-weather-app');
-  });
+	it("should create the app", () => {
+		const fixture = TestBed.createComponent(AppComponent);
+		const app = fixture.componentInstance;
+		expect(app).toBeTruthy();
+	});
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('local-weather-app app is running!');
-  });
+	it(`should have as title 'local-weather-app'`, () => {
+		const fixture = TestBed.createComponent(AppComponent);
+		const app = fixture.componentInstance;
+		expect(app.title).toEqual("local-weather-app");
+	});
+
+	it("should render title", () => {
+		const fixture = TestBed.createComponent(AppComponent);
+		fixture.detectChanges();
+		const compiled = fixture.nativeElement;
+		expect(compiled.querySelector(".content span").textContent).toContain(
+			"local-weather-app app is running!"
+		);
+	});
 });
