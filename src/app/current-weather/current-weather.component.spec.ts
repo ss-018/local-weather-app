3fca9c120ab85e1ceb6451220356f1f249cfbb3f
Sarjodh Dhillon <sarjodhdhillon@Sarjodhs-MacBook-Pro.local>
2020-06-13 15:21:45 -0700

    updated unit tests and e2e tests

--- src/app/current-weather/current-weather.component.spec.ts
+++ src/app/current-weather/current-weather.component.spec.ts
@@ -1,25 +1,67 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
+import { injectSpy } from "angular-unit-test-helper";
+import { of } from "rxjs";
 
-import { CurrentWeatherComponent } from './current-weather.component';
+import { WeatherService } from "../weather/weather.service";
+import { fakeWeather } from "../weather/weather.service.fake";
+import { CurrentWeatherComponent } from "./current-weather.component";
 
-describe('CurrentWeatherComponent', () => {
-  let component: CurrentWeatherComponent;
-  let fixture: ComponentFixture<CurrentWeatherComponent>;
+describe("CurrentWeatherComponent", () => {
+	let component: CurrentWeatherComponent;
+	let fixture: ComponentFixture<CurrentWeatherComponent>;
+	let weatherServiceMock: jasmine.SpyObj<WeatherService>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CurrentWeatherComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(async(() => {
+		const weatherServiceSpy = jasmine.createSpyObj("WeatherService", ["getCurrentWeather"]);
+		TestBed.configureTestingModule({
+			declarations: [CurrentWeatherComponent],
+			providers: [
+				{
+					provide: WeatherService,
+					useValue: weatherServiceSpy,
+				},
+			],
+		}).compileComponents();
+		weatherServiceMock = injectSpy(WeatherService);
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CurrentWeatherComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(CurrentWeatherComponent);
+		component = fixture.componentInstance;
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it("should create", () => {
+		// Arrange
+		weatherServiceMock.getCurrentWeather.and.returnValue(of());
+		// Act
+		fixture.detectChanges(); // triggers ngOnInit
+		// Assert
+		expect(component).toBeTruthy();
+	});
+
+	it("should get currentWeather from weatherService", () => {
+		// Arrange
+		weatherServiceMock.getCurrentWeather.and.returnValue(of());
+		// Act
+		fixture.detectChanges();
+		// Assert
+		expect(weatherServiceMock.getCurrentWeather).toHaveBeenCalledTimes(1);
+	});
+
+	it("should eagerly load currentWeather in Seattle from weatherService", () => {
+		// Arrange
+		weatherServiceMock.getCurrentWeather.and.returnValue(of(fakeWeather));
+		// Act
+		fixture.detectChanges();
+		// Assert
+		expect(component.current).toBeDefined();
+		expect(component.current.city).toEqual("Seattle");
+		expect(component.current.temperature).toEqual(280.32);
+
+		// Assert on DOM
+		const debugE1 = fixture.debugElement;
+		const titleE1: HTMLElement = debugE1.query(By.css("span")).nativeElement;
+		expect(titleE1.textContent).toContain("Seattle");
+	});
 });
--- src/app/weather/weather.service.spec.ts
+++ src/app/weather/weather.service.spec.ts
@@ -1,16 +1,19 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { TestBed } from "@angular/core/testing";
 
-import { WeatherService } from './weather.service';
+import { WeatherService } from "./weather.service";
 
-describe('WeatherService', () => {
-  let service: WeatherService;
+describe("WeatherService", () => {
+	let service: WeatherService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(WeatherService);
-  });
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			imports: [HttpClientTestingModule],
+		});
+		service = TestBed.inject(WeatherService);
+	});
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+	it("should be created", () => {
+		expect(service).toBeTruthy();
+	});
 });
